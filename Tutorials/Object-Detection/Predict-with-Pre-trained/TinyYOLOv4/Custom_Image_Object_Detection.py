64f8ad0011d21f2ae275220d6090abfd90d5a930
Adeel <56041154+Adeel-Intizar@users.noreply.github.com>
2020-11-01 09:36:49 +0500

    Add files via upload

---
+++ Tutorials/Object-Detection/Predict-with-Pre-trained/TinyYOLOv4/Custom_Image_Object_Detection.py
@@ -0,0 +1,11 @@
+from xtreme_vision.Detection import Object_Detection
+from PIL import Image
+
+model = Object_Detection()
+model.Use_TinyYOLOv4()
+custom_objects = model.Custom_Objects(kite=True, person=False)
+model.Detect_Custom_Objects_From_Image(custom_objects, 
+                                       input_path = 'kite.jpg',
+                                       output_path = './out.jpg')
+
+Image.open('out.jpg')
---
+++ Tutorials/Object-Detection/Predict-with-Pre-trained/TinyYOLOv4/Video_Object_Detection.py
@@ -0,0 +1,6 @@
+from xtreme_vision.Detection import Object_Detection
+
+model = Object_Detection()
+model.Use_TinyYOLOv4()
+model.Detect_From_Video(input_path = 'road.mp4', 
+                        output_path='./out.mp4')
